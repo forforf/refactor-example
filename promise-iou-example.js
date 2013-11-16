4a22755113505859a71b5d424ff3bdf53766923d
Dave <dmarti21@gmail.com>
2013-11-16 14:35:06 -0500

    creates an Acct class for tracking account

--- promise-iou-example.js
+++ promise-iou-example.js
@@ -3,52 +3,45 @@ var Q = require('q')
 var giveIou = require('./lib/iou.js').giveIou;
 var giveIouTo = require('./lib/iou.js').giveIouTo;
 
-//our money shouldn't be infinite
-var myAcct = 10000;
-console.log('My Acct started with: $', myAcct);
+//Lets make an account a function with a method for deducting
+//and use it to track our money.
+function Acct(name, initialDeposit){
+  var balance = initialDeposit;
+  this.name = function(){ return name };
+  this.balance = function(){ return balance };
+  this.withdraw = function(amt) {
+    balance = balance - amt;
+    console.log('Balance for', name, 'is $', balance);
+    return balance;
+  }
+}  
+var myAcct = new Acct('me', 10000);
 
-//and we should deduct when we pay an IOU
+console.log('My Acct started with: $', myAcct.balance());
+
+// note that the function invoked by then takes the iouAmt parameter
 giveIouTo('Alex', giveIou(10, 1000))
   .then( function(iouAmt){
-    myAcct = myAcct - iouAmt;
-    console.log('My Acct: $', myAcct);
+    myAcct.withdraw(iouAmt);
+    console.log('My Acct: $', myAcct.balance());
   })
 ;
+
+// lets replace it with the withdraw method from Acct class
 giveIouTo('Beth', giveIou(20, 2000))
-  .then( function(iouAmt){
-    myAcct = myAcct - iouAmt;
-    console.log('My Acct: $', myAcct);
-  })
+  .then( myAcct.withdraw )
 ;
+
 giveIouTo('Carl', giveIou(30, 3000))
-  .then( function(iouAmt){
-    myAcct = myAcct - iouAmt;
-    console.log('My Acct: $', myAcct);
-  })
+  .then( myAcct.withdraw )
 ;
+
 giveIouTo('Dave', giveIou(40, 4000))
   .then( function(iouAmt){
     console.log('Thanks Dave, your $', iouAmt, ' really got me out of a jam');
   }) 
 ;  
 giveIouTo('Dave', giveIou(40, 4000))
-  .then( function(iouAmt){
-    myAcct = myAcct - iouAmt;
-    console.log('My Acct: $', myAcct);
-  })
-
-/*
-Note: this pseudo code works:
-giveIouTo().then( 'do things')
-giveIouTo().then( 'do more things')
-
-This pseudo code does NOT work
-giveIouTo().then('do things').then('do more things')
-
-because the first '.then' returns resolved iou
-if the first '.then' resolved to another promise, then
-the second approach would work
-
-*/
-
+  .then( myAcct.withdraw )
+;  
 
