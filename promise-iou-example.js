c52ceafdff38344a83e42daac3f803ecf91ce1cd
Dave <dmarti21@gmail.com>
2013-11-16 15:04:12 -0500

    Refactored individuals to act on a collection

--- promise-iou-example.js
+++ promise-iou-example.js
@@ -1,34 +1,43 @@
-// we actually didn't need once iou was moved to its own lib
-//var Q = require('q')
-
 var giveIou = require('./lib/iou.js').giveIou;
 var giveIouTo = require('./lib/iou.js').giveIouTo;
 var Acct = require('./lib/acct.js');
   
 var myAcct = new Acct('me', 10000);
 
-console.log('My Acct started with: $', myAcct.balance());
+//lets move this to Acct's constructor rather than main
+//console.log('My Acct started with: $', myAcct.balance());
 
-// note that the function invoked by then takes the iouAmt parameter
-giveIouTo('Alex', giveIou(10, 1000))
-  .then( myAcct.withdraw )
-;
 
-// lets replace it with the withdraw method from Acct class
-giveIouTo('Beth', giveIou(20, 2000))
-  .then( myAcct.withdraw )
-;
+// We could almost interate over Alex, Beth, Carl and Dave
+// and insert their names into the giveIouTo function's first arg
+// If not for the special message for Dave
 
-giveIouTo('Carl', giveIou(30, 3000))
-  .then( myAcct.withdraw )
-;
+// however, we can create a map of names, iuos and a list of thennable
+// functions and iterate over that to simplify things
 
-giveIouTo('Dave', giveIou(40, 4000))
-  .then( function(iouAmt){
-    console.log('Thanks Dave, your $', iouAmt, ' really got me out of a jam');
-  }) 
-;  
-giveIouTo('Dave', giveIou(40, 4000))
-  .then( myAcct.withdraw )
-;  
+// and a generic thank you function, but it has to accept the amt parameter
+// since our thennable functions are just functions without any parameters
+function thanks(iouAmt){
+  console.log('Thank you sir/madam for your loan of  $', iouAmt, ' kind regards');
+}  
+
+var myIous = {
+  Alex: {amt: 10, dur: 1000, whenPaid: [myAcct.withdraw]},
+  Beth: {amt: 20, dur: 2000, whenPaid: [myAcct.withdraw]},
+  Carl: {amt: 30, dur: 3000, whenPaid: [myAcct.withdraw]},
+  Dave: {amt: 40, dur: 4000, whenPaid: [myAcct.withdraw, thanks]}
+}
+
+//This takes the place of listing each person individually
+Object.keys(myIous).forEach(function(name){
+  var iou = giveIouTo(name, giveIou(myIous[name].amt, myIous[name].dur))
+  
+  myIous[name].whenPaid.forEach(function(payFunction){
+    iou.then(payFunction);
+  });
+});
+
+
+
+
 
