4efaf6aa6ada971335f27bb894f00949585443f4
Dave <dmarti21@gmail.com>
2013-11-16 13:32:02 -0500

    returning iou along, so we can chain on its reponse

--- promise-iou-example.js
+++ promise-iou-example.js
@@ -7,7 +7,13 @@ var giveIouTo = require('./lib/iou.js').giveIouTo;
 giveIouTo('Alex', giveIou(10, 1000));
 giveIouTo('Beth', giveIou(20, 2000));
 giveIouTo('Carl', giveIou(30, 3000));
-giveIouTo('Dave', giveIou(40, 4000));
+
+//giveIouTo now returns the iou promise and can be used
+giveIouTo('Dave', giveIou(40, 4000))
+  .then( function(iouAmt){
+    console.log('Thanks Dave, your $', iouAmt, ' really got me out of a jam');
+  })
+;  
 
 
 
