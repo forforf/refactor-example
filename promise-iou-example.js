8e70eba1bd270ef60f4d41b3b1ef21a8b1331245
Dave <dmarti21@gmail.com>
2013-11-17 11:38:38 -0500

    refactored to iou class

--- promise-iou-example.js
+++ promise-iou-example.js
@@ -4,40 +4,60 @@ var Acct = require('./lib/acct.js');
   
 var myAcct = new Acct('me', 10000);
 
-//lets move this to Acct's constructor rather than main
-//console.log('My Acct started with: $', myAcct.balance());
+// Ok, this is much more concise, but there are two issues
+//   1) readability is pretty bad
+//   2) We lost our individualized thanks.
+// Is there a way to fix this?
 
+// What if we create a IouRecipient Class?
 
-// We could almost interate over Alex, Beth, Carl and Dave
-// and insert their names into the giveIouTo function's first arg
-// If not for the special message for Dave
+function Iou(recipient, amt, dur, whenPaids) {
+  var _this = this; //handle for this object when created
 
-// however, we can create a map of names, iuos and a list of thennable
-// functions and iterate over that to simplify things
+  this.payFuncs = whenPaids;
+  this.recipient = function(){ return recipient; };
+  this.amt = function(){ return amt; };
+  this.duration = function(){ return dur; };
 
-// and a generic thank you function, but it has to accept the amt parameter
-// since our thennable functions are just functions without any parameters
-function thanks(iouAmt){
-  console.log('Thank you sir/madam for your loan of  $', iouAmt, ' kind regards');
-}  
-
-var myIous = {
-  Alex: {amt: 10, dur: 1000, whenPaid: [myAcct.withdraw]},
-  Beth: {amt: 20, dur: 2000, whenPaid: [myAcct.withdraw]},
-  Carl: {amt: 30, dur: 3000, whenPaid: [myAcct.withdraw]},
-  Dave: {amt: 40, dur: 4000, whenPaid: [myAcct.withdraw, thanks]}
+  //this works, but it has issues to address  
+  this.give = function(){
+    return giveIouTo( _this.recipient(), giveIou(_this.amt(), _this.duration()));
+  }
+  
+  this.whenPaidFn = function(){
+    var whenPaid = function(iouAmt){
+      _this.payFuncs.forEach( function(payFunc) {
+        // allows us to pass self-referencing object to whenPaids
+        payFunc(iouAmt, _this);
+      });
+    };
+    return whenPaid;
+  }
 }
 
+//now we can customize the thanks fn
+function thanks(iouAmt, iou){
+  console.log('Thanks', iou.recipient(), 'for your loan of  $', iouAmt);
+}  
+
+function jamMsg(){
+  console.log('It got me out of a real Jam');
+}
+
+//myAcct.withdraw still works, because we kept the iouAmt the first arg
+var myIous = [
+  new Iou('Alex', 10, 1000, [myAcct.withdraw]),
+  new Iou('Beth', 20, 2000, [myAcct.withdraw]),
+  new Iou('Carl', 30, 3000, [myAcct.withdraw]),
+  new Iou('Dave', 40, 4000, [myAcct.withdraw, thanks, jamMsg])
+]
+
 //This takes the place of listing each person individually
-Object.keys(myIous).forEach(function(name){
-  var iou = giveIouTo(name, giveIou(myIous[name].amt, myIous[name].dur))
-  
-  myIous[name].whenPaid.forEach(function(payFunction){
-    iou.then(payFunction);
+myIous.forEach(function(iou){
+  iou.give().then(function(amt){
+    iou.whenPaidFn()(amt);
   });
 });
-
-
 
 
 
